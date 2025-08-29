f10a90729bf0992dbf184095c3ff2238fd7ae226
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2025-08-28 19:16:35 -0700

    Create triple store schema

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+export type HLC = string;
+
+export type Subject = string;
+export type Predicate = string;
+export type Value = string | number | boolean | Date | null;
+export type Ref = { id: string };
+export type RefMany = Ref[];
+
+export type Object = Value | Ref | RefMany;
+export type Triple = [Subject, Predicate, Object, HLC];
