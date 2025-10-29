f2a411d22b1b2ce7e30e574aed05b55d47633679
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2025-10-28 23:20:59 -0700

    Create test for bundle size

---
+++ packages/client/bundle-size.test.ts
@@ -0,0 +1,28 @@
+import { expect, it } from "bun:test";
+import { brotliCompressSync } from "node:zlib";
+
+it("uncompressed bundle size must not exceed 40kb", async () => {
+	await Bun.build({
+		entrypoints: ["./packages/client/index.ts"],
+		outdir: "./dist/uncompressed",
+		minify: true,
+		format: "esm",
+	});
+	const bundle = Bun.file("dist/uncompressed/index.js");
+	console.log(`Uncompressed bundle size is ${bundle.size / 1000}kb`);
+	expect(bundle.size).toBeLessThan(10_000);
+});
+
+it("compressed bundle size must not exceed 10kb", async () => {
+	await Bun.build({
+		entrypoints: ["./packages/client/index.ts"],
+		outdir: "./dist/compressed",
+		minify: true,
+		format: "esm",
+	});
+	const bundle = Bun.file("dist/compressed/index.js");
+	const bytes = await bundle.bytes();
+	const compressed = brotliCompressSync(bytes);
+	console.log(`Compressed bundle size is ${compressed.length / 1000}kb`);
+	expect(compressed.length).toBeLessThan(10_000);
+});
