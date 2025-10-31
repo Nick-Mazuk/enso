4a3c392e668f64c25f726e4168c1a39159c16b15
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2025-10-31 07:09:34 -0700

    Reorganize query tests

--- packages/client/internal/database/create.test.ts
+++ packages/client/internal/database/create.test.ts
@@ -2,7 +2,6 @@ import { describe, expect, expectTypeOf, it } from "bun:test";
 import { createSchema, t } from "../../index";
 import { Store } from "../store";
 import { createDatabase } from "./create";
-import type { DatabaseResult } from "./types";
 
 describe("createDatabase", () => {
 	it("creates a database the correct keys", () => {
@@ -89,35 +88,7 @@ describe("database.entity.query", () => {
 		>();
 	});
 
-	// DO NOT SUBMIT: reorganize the tests by scenario, not "type vs. functionality".
-	// That'll make this more extensible to future scenarios.
-	it("return type is inferred correctly", () => {
-		const schema = createSchema({
-			entities: {
-				users: {
-					name: t.string({ fallback: "" }),
-					age: t.number({ optional: true }),
-				},
-			},
-		});
-		const store = new Store();
-		const database = createDatabase(schema, store);
-
-		const result1 = database.users.query({ fields: { age: true, name: true } });
-		expectTypeOf(result1).toEqualTypeOf<
-			DatabaseResult<{ name: string; age?: number }[]>
-		>();
-
-		const result2 = database.users.query({ fields: { id: true } });
-		expectTypeOf(result2).toEqualTypeOf<DatabaseResult<{ id: string }[]>>();
-
-		const result3 = database.users.query({ fields: { name: true, id: true } });
-		expectTypeOf(result3).toEqualTypeOf<
-			DatabaseResult<{ name: string; id: string }[]>
-		>();
-	});
-
-	it("return the correct data", () => {
+	it("can query for all schema fields", () => {
 		const schema = createSchema({
 			entities: {
 				users: {
@@ -130,24 +101,75 @@ describe("database.entity.query", () => {
 		const database = createDatabase(schema, store);
 
 		database.users.create({ name: "John Doe", age: 30 });
-		database.users.create({ name: "Jane Doe" });
 
-		const result1 = database.users.query({ fields: { age: true, name: true } });
-		expect(result1).toEqual({
-			data: [{ name: "John Doe", age: 30 }, { name: "Jane Doe" }],
+		const result = database.users.query({ fields: { age: true, name: true } });
+		expect(result).toEqual({
+			data: [{ name: "John Doe", age: 30 }],
 		});
+	});
 
-		const result2 = database.users.query({ fields: { id: true } });
-		expect(result2).toEqual({
-			data: [{ id: expect.any(String) }, { id: expect.any(String) }],
+	it("objects with missing optional fields are still returned", () => {
+		const schema = createSchema({
+			entities: {
+				users: {
+					name: t.string({ fallback: "" }),
+					age: t.number({ optional: true }),
+				},
+			},
 		});
+		const store = new Store();
+		const database = createDatabase(schema, store);
 
-		const result3 = database.users.query({ fields: { name: true, id: true } });
-		expect(result3).toEqual({
-			data: [
-				{ id: expect.any(String), name: "John Doe" },
-				{ id: expect.any(String), name: "Jane Doe" },
-			],
+		database.users.create({ name: "John Doe", age: 30 });
+		database.users.create({ name: "John Doe" });
+
+		const result = database.users.query({ fields: { age: true, name: true } });
+		expect(result).toEqual({
+			data: [{ name: "John Doe", age: 30 }, { name: "John Doe" }],
+		});
+	});
+
+	it("can query for the auto-generated id", () => {
+		const schema = createSchema({
+			entities: {
+				users: {
+					name: t.string({ fallback: "" }),
+					age: t.number({ optional: true }),
+				},
+			},
+		});
+		const store = new Store();
+		const database = createDatabase(schema, store);
+
+		database.users.create({ name: "John Doe", age: 30 });
+
+		const result = database.users.query({ fields: { id: true } });
+		expect(result).toEqual({
+			data: [{ id: expect.any(String) }],
+		});
+	});
+
+	it("can query for auto-generated and schema fields in the same query", () => {
+		it("can query for all schema fields", () => {
+			const schema = createSchema({
+				entities: {
+					users: {
+						name: t.string({ fallback: "" }),
+						age: t.number({ optional: true }),
+					},
+				},
+			});
+			const store = new Store();
+			const database = createDatabase(schema, store);
+
+			database.users.create({ name: "John Doe", age: 30 });
+
+			const result = database.users.query({
+				fields: { name: true, id: true },
+			});
+			expect(result).toEqual({
+				data: [{ id: expect.any(String), name: "John Doe" }],
+			});
 		});
 	});
 });
